29c9b39a159728ffbdf13b1d5496862263b7904e
Ehsan Ansari <ehsan.ansari21@smslucknow.ac.in>
2025-01-10 21:08:22 +0530

    altering animation

--- PopUp/index.js
+++ PopUp/index.js
@@ -1,14 +1,33 @@
 const button = document.querySelector('button');
 const popup= document.querySelector('.popup');
 const closeicon = document.querySelector('.close-icon');
+const popupContainer = document.querySelector('.popup-container');
 console.log(button);
 
 
-button.addEventListener('click',()=>{
+button.addEventListener('click',(e)=>{
     popup.classList.add('open')
     popup.classList.remove('close');
+    popupContainer.classList.remove('close-popup-container')
+    
+    e.stopPropagation();
 })
-closeicon.addEventListener('click',()=>{
+closeicon.addEventListener('click',(e)=>{
     popup.classList.remove('open');
     popup.classList.add('close');
+    popupContainer.classList.add('close-popup-container')
+})
+
+popup.addEventListener('click',(e)=>{
+    event.stopPropagation();
+
+})
+
+
+popupContainer.addEventListener('click',()=>{
+    popup.classList.remove('open');
+    popup.classList.add('close');
+    console.log('hi');
+    popupContainer.classList.add('close-popup-container')
+    
 })
